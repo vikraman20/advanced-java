39567935b07954473a374bccb7c88de8ee5c6c14
vikraman20 <110642866+vikraman20@users.noreply.github.com>
2022-09-19 23:12:45 -0400

    Update App.java

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -4,6 +4,6 @@ package app;
 class App {
     public static void main (String[] args)
     {
-            System.out.println("Hello World");
+            System.out.println("Hello Vaanavil");
     }
 }
